287b3057b97921667e280dceb2a30141b4a6a46d
jean TROUSSIER <jean.troussier@cpe.fr>
2024-09-19 10:58:32 +0200

    fianl change

--- impot/fichierprincipale.py
+++ impot/fichierprincipale.py
@@ -4,3 +4,11 @@ date : jeudi 19 septembre
 objectif : fonction principale de la gestion d'impot
 ToDo : interface, fonction de traitement du montant a payer, des tranches)
 """
+import fonctionimpot as fi
+print("please input your salary")
+reponse = input("repones utilisateur :")
+argent_perdu = fi.impot_a_calc(reponse)
+if argent_perdu == -1:
+    print("veuiller entrée une donnée valide")
+else :
+    print("vous devez : ", argent_perdu," d'euro")
--- impot/fonctionimpot.py
+++ impot/fonctionimpot.py
@@ -7,7 +7,12 @@ ToDo : interface, fonction de traitement du montant a payer, des tranches)
 liste_de_tranche = [[0,10225],[10226,26070],[26071,74545],[75546,160336]]
 liste_des_cote = [0,11,30,41,45]
 
+
+
 def verif_input(input):
+    """
+    fonction qui prend comme paramèter une float et qui renvoie true si c une float ou false si ce n'est pas le cas
+    """
     try:
         float(input)
     except:
@@ -16,21 +21,26 @@ def verif_input(input):
 
 def tranche(float_salaire):
     """
-    fonction qui a pour entrée un int contenant le salaire de salarié et qui retourne sa cote, en int
+    fonction qui a pour entrée une float contenant le salaire de salarié et qui retourne sa cote, en int
     """
     cat = 0
     for i in liste_de_tranche:
-        if int_salaire > i[1]:
+        if float_salaire > i[1]:
             cat +=1
         else:
             return cat
     return cat
 
 def impot_a_calc(int_salaire):
+    """
+    fonction qui prend en entrée une floatante , le salaire et qui renvoie sous forme de floatante, l'impot a payer
+    """
     if verif_input(int_salaire) == False:
         return -1
+    int_salaire = float(int_salaire)
     cat = tranche(int_salaire) 
-    if cat <= liste_de_tranche[0][1]:
+    print(cat)
+    if int_salaire <= liste_de_tranche[0][1]:
         return 0
     else:
-        return (int_salaire - liste_de_tranche[cat - 1] + 1) *  liste_des_cote[cat] / 100
+        return (int_salaire - liste_de_tranche[cat - 1][1] + 1) *  liste_des_cote[cat] / 100
