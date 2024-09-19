38f6e870225e0fffdad3575cb658058faa0b0318
jean TROUSSIER <jean.troussier@cpe.fr>
2024-09-19 10:03:58 +0200

    yo les copaing

---
+++ impot/fonctionimpot.py
@@ -0,0 +1,8 @@
+"""
+user : Jean TROUSSIER
+date : jeudi 19 septembre
+objectif : fonction de la gestion d'impot
+ToDo : interface, fonction de traitement du montant a payer, des tranches)
+"""
+liste_de_tranche = [[0,10225],[10226,26070],[26071,74545],[75546,160336]]
+liste_des_cote = 
