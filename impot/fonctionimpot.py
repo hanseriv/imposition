382785b32bb57dd0b638717608edb5fcb933acf2
jean TROUSSIER <jean.troussier@cpe.fr>
2024-09-19 10:30:22 +0200

    gre

--- impot/fonctionimpot.py
+++ impot/fonctionimpot.py
@@ -5,4 +5,4 @@ objectif : fonction de la gestion d'impot
 ToDo : interface, fonction de traitement du montant a payer, des tranches)
 """
 liste_de_tranche = [[0,10225],[10226,26070],[26071,74545],[75546,160336]]
-liste_des_cote = 
+liste_des_cote = [0,11,30,41,45]
