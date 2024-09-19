0cc2a404ad0028786de6cb2c4f024a9df8383cfb
jean TROUSSIER <jean.troussier@cpe.fr>
2024-09-19 10:33:54 +0200

    please help

--- impot/fonctionimpot.py
+++ impot/fonctionimpot.py
@@ -6,3 +6,8 @@ ToDo : interface, fonction de traitement du montant a payer, des tranches)
 """
 liste_de_tranche = [[0,10225],[10226,26070],[26071,74545],[75546,160336]]
 liste_des_cote = [0,11,30,41,45]
+
+def montant_a_payer(int_salaire):
+    """
+    fonction qui a pour entrée un int contenant le salaire de salarié et qui retourne sa cote, en int
+    """
